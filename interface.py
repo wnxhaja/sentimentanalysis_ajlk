946319d560ede9038ffebe44e060f8fe8a936bef
loway1673 <louiebasay@gmail.com>
2015-07-06 23:13:41 +0800

    Edited the output for preprocessing. Commented some lines in interface

--- interface.py
+++ interface.py
@@ -8,9 +8,9 @@ iter = 1
 print("Welcome!!!\n")
 while iter == 1:
 	tweet = raw_input("Please enter a tweet to be analyzed: ")
-	prep = Preprocess() #load Preprocess class
+	#prep = Preprocess() #load Preprocess class
 
-	prep.printTweet(tweet)
+	#prep.printTweet(tweet)
 	iter = input("Please enter 0 to exit and 1 to continue: ")
 	print("\n")
 
