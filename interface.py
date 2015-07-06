3a39717aa135f14a526b3f5a199deb504dcb3df5
wnxhaja <wnxhaja@gmail.com>
2015-07-06 08:48:25 -0400

    Modify interface.py   preprocess input text   generate bigrams using preprocess text

--- interface.py
+++ interface.py
@@ -8,11 +8,21 @@ iter = 1
 print("Welcome!!!\n")
 while iter == 1:
 	tweet = raw_input("Please enter a tweet to be analyzed: ")
-	prep = Preprocess() #load Preprocess class
+	prep = Preprocess(tweet) #load Preprocess class
 
-	prep.printTweet(tweet)
+	#preprocess input data
+	data = prep.preprocess()
+	print data
+
+	#generate bigrams from preprocessed text
+	for item in data:
+		bigrams = FeatureExtract(item[0]).getBigrams
+		item[0] = bigrams
+		print item
+
 	iter = input("Please enter 0 to exit and 1 to continue: ")
 	print("\n")
 
 
 print("Goodbye!!")
+
